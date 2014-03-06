217d2d789bc2e87ca1ea8adfe9afe44b9eb6ba9f
gurindergulati <gurinder.gulati@countersoft.com>
2014-03-06 16:21:08 +0000

    initial setup

---
+++ AutoFollower.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Countersoft.Gemini.Commons.Entity;
+using Countersoft.Gemini.Extensibility.Events;
+using Countersoft.Gemini.Commons.Dto;
+using Countersoft.Gemini.Extensibility.Apps;
+
+namespace AutoFollower
+{
+    [AppType(AppTypeEnum.Event),
+    AppGuid("6A6D919B-EC37-41BC-A398-AF755C87DE41"),
+    AppName("Auto Follower"),
+    AppDescription("Automatically adds the item creator and resources as followers")]
+    public class AutoFollower : IIssueBeforeListener
+    {
+        public Issue BeforeCreate(IssueEventArgs args)
+        {
+            var resources = args.Entity.GetResources();
+
+            resources.RemoveAll(r => r == 0);
+
+            args.Entity.AddWatchers(resources);
+
+            args.Entity.AddWatcher(args.Entity.ReportedBy);
+
+            return args.Entity;
+        }
+
+        public Issue BeforeUpdate(IssueEventArgs args)
+        {
+            var resources = args.Entity.GetResources();
+
+            var watchers = args.Entity.GetWatchers();
+
+            var missingResources = resources.FindAll(r => !watchers.Contains(r) && r != 0);
+
+            if (missingResources.Count > 0)
+            {
+                args.Entity.AddWatchers(missingResources);
+            }
+
+            return args.Entity;
+        }
+
+        public Issue BeforeDelete(IssueEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public IssueComment BeforeComment(IssueCommentEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public Issue BeforeStatusChange(IssueEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public Issue BeforeResolutionChange(IssueEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public Issue BeforeAssign(IssueEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public Issue BeforeClose(IssueEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public Issue BeforeWatcherAdd(IssueEventArgs args)
+        {
+            return args.Entity;
+        }
+
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string AppGuid { get; set; }
+
+        public IssueDto BeforeCreateFull(IssueDtoEventArgs args)
+        {
+            return args.Issue;
+        }
+
+        public IssueDto BeforeUpdateFull(IssueDtoEventArgs args)
+        {
+            return args.Issue;
+        }
+
+        public IssueDto BeforeIssueCopy(IssueDtoEventArgs args)
+        {
+            return args.Issue;
+        }
+
+        public IssueDto CopyIssueComplete(IssueDtoEventArgs args)
+        {
+            return args.Issue;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoFollower")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoFollower")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da2f1840-5147-480f-9860-b09d8f074243")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("")]
+[assembly: AssemblyVersion("6.1.0.4081")]
+[assembly: AssemblyFileVersion("6.1.0.4081")]
